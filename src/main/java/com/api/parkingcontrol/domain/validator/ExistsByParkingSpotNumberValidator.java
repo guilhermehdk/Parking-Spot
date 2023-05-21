0ece8f11667e9d69eb2394ca41de9856bcd48a34
Guilherme <guilhermehdk@gmail.com>
2023-05-21 20:37:21 -0300

    validações aplicadas para update de dados

--- src/main/java/com/api/parkingcontrol/domain/validator/ExistsByParkingSpotNumberValidator.java
+++ src/main/java/com/api/parkingcontrol/domain/validator/ExistsByParkingSpotNumberValidator.java
@@ -1,6 +1,7 @@
 package com.api.parkingcontrol.domain.validator;
 
 import com.api.parkingcontrol.dtos.request.ParkingSpotRequest;
+import com.api.parkingcontrol.dtos.request.ParkingSpotUpdateRequest;
 import com.api.parkingcontrol.infra.exception.ParkingSpotValidatorException;
 import com.api.parkingcontrol.respositories.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,13 @@ public class ExistsByParkingSpotNumberValidator implements ParkingSpotValidator 
 
     public void validate(ParkingSpotRequest parkingSpotRequest) {
         if (parkingSpotRepository.existsByParkingSpotNumber(parkingSpotRequest.getParkingSpotNumber())) {
+            throw new ParkingSpotValidatorException("Conflict: Parking Spot is already in use!");
+        }
+    }
+
+    @Override
+    public void validate(ParkingSpotUpdateRequest parkingSpotUpdateRequest) {
+        if (parkingSpotRepository.existsByParkingSpotNumber(parkingSpotUpdateRequest.getParkingSpotNumber())) {
             throw new ParkingSpotValidatorException("Conflict: Parking Spot is already in use!");
         }
     }
--- src/main/java/com/api/parkingcontrol/domain/validator/ValidatorExistsByApartmentAndBlock.java
+++ src/main/java/com/api/parkingcontrol/domain/validator/ValidatorExistsByApartmentAndBlock.java
@@ -1,6 +1,7 @@
 package com.api.parkingcontrol.domain.validator;
 
 import com.api.parkingcontrol.dtos.request.ParkingSpotRequest;
+import com.api.parkingcontrol.dtos.request.ParkingSpotUpdateRequest;
 import com.api.parkingcontrol.infra.exception.ParkingSpotValidatorException;
 import com.api.parkingcontrol.respositories.ParkingSpotRepository;
 import com.api.parkingcontrol.service.ParkingSpotService;
@@ -15,6 +16,13 @@ public class ValidatorExistsByApartmentAndBlock implements ParkingSpotValidator 
 
     public void validate(ParkingSpotRequest parkingSpotRequest) {
         if (parkingSpotRepository.existsByApartmentAndBlock(parkingSpotRequest.getApartment(), parkingSpotRequest.getBlock())) {
+            throw new ParkingSpotValidatorException("Conflict: Parking Spot already registered for this apartment/block!");
+        }
+    }
+
+    @Override
+    public void validate(ParkingSpotUpdateRequest parkingSpotUpdateRequest) {
+        if (parkingSpotRepository.existsByApartmentAndBlock(parkingSpotUpdateRequest.getApartment(), parkingSpotUpdateRequest.getBlock())) {
             throw new ParkingSpotValidatorException("Conflict: Parking Spot already registered for this apartment/block!");
         }
     }
